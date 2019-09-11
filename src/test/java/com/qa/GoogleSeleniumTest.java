de1c1c8f2b09391e84c6188b7632df455f159b6e
Adam-Shawcross <Adam.shawcross@academytrainee.com>
2019-09-11 10:40:24 +0100

    test now picks one picture and right clicks on it

--- src/test/java/com/qa/GoogleSeleniumTest.java
+++ src/test/java/com/qa/GoogleSeleniumTest.java
@@ -4,8 +4,10 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import static org.junit.Assert.assertTrue;
 
@@ -34,9 +36,31 @@ public class GoogleSeleniumTest {
         WebElement searchField = driver.findElement(By.name("q"));
         assertTrue(searchField.isDisplayed());
         searchField.sendKeys("funny cat pics");
-        Thread.sleep(5000);
+        Thread.sleep(2000);
         WebElement submitbutton = driver.findElement(By.name("btnK"));
         submitbutton.click();
-        Thread.sleep(5000);
+        Thread.sleep(2000);
+        WebElement linkToBigCats = driver.findElementByLinkText("Images for funny cat");
+        linkToBigCats.click();
+        Thread.sleep(4000);
+        WebElement cuteCat = driver.findElementById("c06tNVL-sDc9sM:");
+        cuteCat.click();
+
+        Thread.sleep(4000);
+        WebElement cuteCatBig = driver.findElementByClassName("irc_mic");
+        Actions action= new Actions(driver);
+        action.contextClick(cuteCatBig).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.RETURN).build().perform();
+        Thread.sleep(4000);
+
     }
+
+
+
+
+
+
+
+
+
+
 }
