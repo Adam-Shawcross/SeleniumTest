d04ec001f372bc8bfab8b11bcdde60b159e61d39
Adam-Shawcross <Adam.shawcross@academytrainee.com>
2019-09-11 09:36:20 +0100

    initial commit

---
+++ src/test/java/com/qa/GoogleSeleniumTest.java
@@ -0,0 +1,20 @@
+package com.qa;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleSeleniumTest {
+
+    private ChromeDriver driver;
+
+
+    @Before
+
+
+
+    @After
+
+    @Test
+}
