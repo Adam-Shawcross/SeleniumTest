dab80e86ea0b4377dd9f80bc552699fd5eaf9823
Adam-Shawcross <Adam.shawcross@academytrainee.com>
2019-09-11 11:32:55 +0100

    Single checkbox test added

---
+++ src/test/java/com/qa/CheckBoxTest.java
@@ -0,0 +1,44 @@
+package com.qa;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import static org.junit.Assert.assertEquals;
+
+public class CheckBoxTest {
+
+    private ChromeDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\IdeaProjects\\SeleniumTest\\src\\test\\java\\resources\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+
+    @After
+    public void tearDown(){
+        driver.close();
+    }
+
+    @Test
+    public void singleCheckboxTest() throws InterruptedException {
+        driver.get("https://www.seleniumeasy.com/test/basic-checkbox-demo.html");
+        Thread.sleep(2000);
+        WebElement checkbox = driver.findElementById("isAgeSelected");
+        checkbox.click();
+        WebElement showMessage = driver.findElementById("txtAge");
+        assertEquals("Success - Check box is checked", showMessage.getText());
+        Thread.sleep(3000);
+    }
+
+
+
+}
